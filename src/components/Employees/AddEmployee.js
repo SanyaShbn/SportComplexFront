42e3867e6708285d76c73d4d958aa6db4ac9f2f3
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-02-27 22:42:27 +0300

    Update AddEmployee.js

--- src/components/Employees/AddEmployee.js
+++ src/components/Employees/AddEmployee.js
@@ -1,32 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './employeeCSS.css';
 import './table.css';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import Stack from '@mui/material/Stack';
 
-const AddEmployee = () => {
+function AddEmployee(props){
+  const [open, setOpen] = useState(false);
+  const [employee, setEmployee] = useState({
+    firstName: '', surName: '', patrSurName: '',  
+    phoneNumber: '', birthDate:'', salary:'', additionalSalary:''
+  });
+
+
+  // Open the modal form
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+    
+  // Close the modal form 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Save car and close modal form 
+  const handleSave = () => {
+    props.addEmployee(employee);
+    handleClose();
+  }
+
+  const handleChange = (event) => {
+    setEmployee({...employee, [event.target.name]: event.target.value});
+  }
   return (
     <div>
-    <h2>Добавление сотрудников</h2>
-    <table>
-      <thead>
-      </thead>
-      <tbody>
-        <tr>
-            <td><input type='text' placeholder='Введите Имя'></input></td>
-            <td><input type='text' placeholder='Введите Фамилию'></input></td>
-            <td><input type='text' placeholder='Введите Отчество'></input></td>
-            <td><input type='text' placeholder='Введите уровень образования'></input></td>
-            <td><input type='text' placeholder='Введите номер телефона'></input></td>
-            <td><input type='date' placeholder='Введите дату рождения'></input></td>
-            <td><input type='number' placeholder='Введите размер оклада'></input></td>
-            <td><input type='number' placeholder='Введите размер премиальных'></input></td>
-            <td>
-            <button type="submit" className="shine-button"><i>Добавить</i>
-                </button>
-            </td>
-        </tr>
-      </tbody>
-    </table>
-    </div>
+    <Button className="shine-button" variant="contained" onClick={handleClickOpen}>
+      Добавить информацию
+    </Button>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle className='dialog'>Новый сотрудник</DialogTitle>
+      <DialogContent className='dialog'>
+        <Stack spacing={2} mt={1}>
+          <TextField label="Имя" name="firstName" autoFocus
+            variant="standard" value={employee.firstName} 
+            onChange={handleChange}/>
+           <TextField label="Фамилия" name="surName"
+            variant="standard" value={employee.surName} 
+            onChange={handleChange}/>
+          <TextField label="Отчество" name="patrSurName" 
+            variant="standard" value={employee.patrSurName} 
+            onChange={handleChange}/>
+          <TextField label="Номер телефона" name="phoneNumber" 
+            variant="standard" value={employee.phoneNumber} 
+            onChange={handleChange}/>
+          <TextField type='date' label="Дата рождения" name="birthDate" 
+            variant="standard" value={employee.birthDate} 
+            onChange={handleChange} InputProps={{
+              inputProps: {
+                inputMode: 'numeric',
+              },
+              startAdornment: (
+                <InputAdornment position="start"> </InputAdornment>
+              ),
+            }}/>
+          <TextField label="Оклад" name="salary"
+            variant="standard" value={employee.salary} 
+            onChange={handleChange}/>
+           <TextField label="Премиальные" name="additionalSalary"
+            variant="standard" value={employee.additionalSalary} 
+            onChange={handleChange}/>
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+         <Button onClick={handleClose}>Отмена</Button>
+         <Button onClick={handleSave}>Добавить</Button>
+      </DialogActions>
+    </Dialog>            
+  </div>
   );
 }
 
