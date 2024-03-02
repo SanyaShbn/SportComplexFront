dbb2d0d2c55f8f9f92b60678fcf3008dd22304bc
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-03-02 23:16:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import './App.css';
 import Login from './components/Authentication/Login';
 function App() {
   return (
-    <div className="App">
-      <div className="main_nav">
-       <a href="/admin_main" className="navigation_element">Главная страница</a>
-       <a href="/" className="navigation_element">Клиенты</a>
-       <a href="/" className="navigation_element">Сооружения комплекса</a>
-      </div>
-      <h1>Сотрудники спортивно-оздоровительного комплекса</h1>
-      <h2>Сотрудники, зарегестрированные в системе</h2>
-      <Login />
+    <div>
+      <Login/>
     </div>
   );
 }
