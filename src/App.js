5e8ef4c0d5d954c320b585f6848693e82261087c
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-03-02 10:51:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 
 import React from 'react';
 import './App.css';
-import EmployeeTable from './components/Employees/EmployeeTable';
-
+import Login from './components/Authentication/Login';
 function App() {
   return (
     <div className="App">
       <div className="main_nav">
-       <a href="/" className="navigation_element">Главная страница</a>
+       <a href="/admin_main" className="navigation_element">Главная страница</a>
        <a href="/" className="navigation_element">Клиенты</a>
        <a href="/" className="navigation_element">Сооружения комплекса</a>
       </div>
       <h1>Сотрудники спортивно-оздоровительного комплекса</h1>
       <h2>Сотрудники, зарегестрированные в системе</h2>
-      <EmployeeTable />
+      <Login />
     </div>
   );
 }
