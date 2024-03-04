80d1791705e86438c41e795348924b2cfa52d51c
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-03-04 21:25:50 +0300

    Update AdminMain.js

--- src/components/MainNavs/AdminMain.js
+++ src/components/MainNavs/AdminMain.js
@@ -12,57 +12,58 @@ import handshake from "./nav_images/handshake.png";
 
 function AdminMain() {
 
-  const fetchAdminMain = () => {
-    fetch('api/admin')
-  }
+//   const fetchAdminMain = () => {
+//     fetch('api/admin')
+//   }
   return (
     <div className='admin_main_body'>
        <style>
         @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
       </style>
-    <div class="top">
-    <div class="logout"></div>
-           <div class="logo_div">
-               <img src= {logo} class="logo"/>
+    <div className="top">
+    <div className="logout"></div>
+           <div className="logo_div">
+               <img src= {logo} className="logo"/>
            </div>
-       <a class="logout_link">Выход</a>
+           <a className="logout_link">Выход</a>
      </div>
-<div class="welcome_text">
-   <p>
+<div className="welcome_text">
+   <p className="welcome_phrase">
     Добро пожаловать
    </p>
-   <p class="info">
+   <p className="info">
     Система управления спортивно-оздоровительным комплексом
     <br/>
-    <span>BestSports</span>
+    <span className='company_title'>BestSports</span>
    </p>
 </div>
-<div class="about_us">
-   <div class="element_1">
-       <img src={admin_logo} class="admin_logos"/>
-       <p class="image1_text">Администрирование</p>
+<div className="about_us">
+   <div className="element_1">
+       <img src={admin_logo} className="admin_logos"/>
+       <p className="image1_text">Администрирование</p>
    </div> 
-   <div class="element_2">
-       <img src={employees} class="admin_logos"/>
-        <p class="image2_text">Работа с сотрудниками</p>
+   <div className="element_2">
+       <img src={employees} className="admin_logos"/>
+        <p className="image2_text">Работа с сотрудниками</p>
    </div>
-   <div class="element_3">
-       <img src={clients} class="admin_logos"/>
-       <p class="image3_text">Работа с клиентами</p>
+   <div className="element_3">
+       <img src={clients} className="admin_logos"/>
+       <p className="image3_text">Работа с клиентами</p>
    </div>
 </div>
-<div class="nav_div_1">
-   <img src={employees_nav_logo} class="link_logo"/>
-   <a href="employees" class="navigation_element">Сотрудники</a>
+<div className="nav_div_1">
+   <img src={employees_nav_logo} className="link_logo"/>
+   <a href="service_employees" className="navigation_element">Сотрудники</a>
 </div> 
-<div class="nav_div_2">
-   <img src={handshake} class="link_logo"/>
-   <a href="facilities" class="navigation_element">Клиенты</a>
+<div className="nav_div_2">
+   <img src={handshake} className="link_logo"/>
+   <a href="facilities" className="navigation_element">Клиенты</a>
 </div>
-<div class="nav_div_3">
-   <img src={ball_image} class="link_logo"/>
-   <a href="facilities" class="navigation_element">Сооружения комплекса</a>
+<div className="nav_div_3">
+   <img src={ball_image} className="link_logo"/>
+   <a href="facilities" className="navigation_element">Сооружения комплекса</a>
 </div>
+
 </div>
   );
 }
