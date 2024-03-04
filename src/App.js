d2a577956da2aa2e1d713a130b7733a2a54d0eda
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-03-04 21:24:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import React from 'react';
 import './App.css';
-import Login from './components/Authentication/Login';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+// import Login from './components/Authentication/Login';
+import AdminMain from './components/MainNavs/AdminMain';
+import EmployeeTable from './components/Employees/EmployeeTable';
+// import AccountantMain from './components/MainNavs/AccountantMain';
+// import ManagerMain from './components/MainNavs/ManagerMain';
+// import CoachMain from './components/MainNavs/CoachMain';
+// import MarketerMain from './components/MainNavs/MarketerMain';
+
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<AdminMain/> } />
+          <Route path="service_employees" element={<EmployeeTable />} />
+          <Route path="admin_main" element={<AdminMain />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
