b6c280a4deddba3b46c8b94c491a873c2bc798c9
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-03-17 14:14:34 +0300

    Create Dashboard.js

---
+++ src/components/MainPage/Dashboard.js
@@ -0,0 +1,93 @@
+import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
+import {
+  Box,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  IconButton,
+} from '@mui/material';
+import MuiAppBar from '@mui/material/AppBar';
+import { Brightness4, Brightness7, Menu } from '@mui/icons-material';
+import { useMemo, useState } from 'react';
+import SideList from './SideList';
+import logo from '../MainNavs/nav_images/logo.png'
+import '../CSS/main.css';
+
+const drawerWidth = 250;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default function Dashboard() {
+  const [open, setOpen] = useState(false);
+  const [dark, setDark] = useState(true);
+
+  const darkTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: dark ? 'dark' : 'light',
+        },
+      }),
+    [dark]
+  );
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+    </style>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 5,
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <Menu />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+            <img src= {logo} className='system_logo'/>
+            <span className='company_title'>BestSports</span>
+            </Typography>
+            <IconButton onClick={() => setDark(!dark)}>
+              {dark ? <Brightness7 /> : <Brightness4 />}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <SideList {...{ open, setOpen }} />
+      </Box>
+    </ThemeProvider>
+  );
+}
