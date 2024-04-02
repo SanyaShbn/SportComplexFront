76eb44f60940506373da300c81f4decca3de04a1
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-04-03 01:16:55 +0300

    Update EditTrainingMembership.js

--- src/components/Reception/TrainingMembership/EditTrainingMembership.js
+++ src/components/Reception/TrainingMembership/EditTrainingMembership.js
@@ -16,8 +16,9 @@ function EditTrainingMembership(props) {
   const [memberships, setMemberships] = useState([]);
   const [trainings, setTrainings] = useState([]);
   const [open, setOpen] = useState(false);
-  const [membership, setMembership] = useState({
-    visitsAmount: ''
+  const [updatedVisitsAmount, setVisitsAmount] = useState([]);
+  const [training_membership, setTrainingMembership] = useState({
+    visitsAmount: '', sportComplexMembership: '', training: ''
   });
     
   useEffect(() => {
@@ -26,19 +27,19 @@ function EditTrainingMembership(props) {
   }, []);
 
   const fetchTrainings = () => {
-      // const token = sessionStorage.getItem("jwt");
-      fetch(SERVER_URL + '/api/trainings', {
-        // headers: { 'Authorization' : token }
-      })
-      .then(response => response.json())
-      .then(data => setTrainings(data._embedded.trainings))
-      .catch(err => console.error(err));    
-    }
+    const token = sessionStorage.getItem("jwt");
+    fetch(SERVER_URL + '/api/view_trainings', {
+      headers: { 'Authorization' : token }
+    })
+    .then(response => response.json())
+    .then(data => setTrainings(data))
+    .catch(err => console.error(err));    
+  }
 
     const fetchMemberships = () => {
-        // const token = sessionStorage.getItem("jwt");
+        const token = sessionStorage.getItem("jwt");
         fetch(SERVER_URL + '/api/view_memberships', {
-          // headers: { 'Authorization' : token }
+          headers: { 'Authorization' : token }
         })
         .then(response => response.json())
         .then(data => setMemberships(data))
@@ -47,23 +48,50 @@ function EditTrainingMembership(props) {
 
 
     const handleClickOpen = () => {
-        setMembership({
-            visitsAmount: props.data.row.visitsAmount
-         })      
-        setOpen(true);
+      let id_membership = props.data.row.sportComplexMembership.slice(props.data.row.sportComplexMembership.indexOf("№") + 1, 
+      props.data.row.sportComplexMembership.indexOf(":"))
+      let id_training = props.data.row.training.slice(props.data.row.training.indexOf("№") + 1, props.data.row.training.indexOf("."))
+      setMembershipId(parseInt(id_membership))
+      setTrainingId(parseInt(id_training))
+      setTrainingMembership({
+        visitsAmount: props.data.row.visitsAmount,
+        sportComplexMembership: parseInt(id_membership),
+        training: parseInt(id_training),
+      })          
+      setOpen(true);
       }
       
-      const handleChange = (event) => {
-        setMembership({...membership, 
-          [event.target.name]: event.target.value});
-      }
+  const handleChangeMembership = (event) => {
+    setMembershipId(event.target.value) 
+    setTrainingMembership({...training_membership, 
+      [event.target.name]: event.target.value});
+  }
+
+  const handleChangeTraining = (event) => {
+    setTrainingId(event.target.value) 
+    setTrainingMembership({...training_membership, 
+      [event.target.name]: event.target.value});
+  }
+
+  const handleChangeAmount= (event) => {
+    setVisitsAmount(event.target.value)
+    setTrainingMembership({...training_membership, 
+      [event.target.name]: event.target.value});
+  }
 
   const handleClose = () => {
     setOpen(false);
   };
  
   const handleSave = () => {
-    props.updateTrainingMembership(membership, props.data.id, trainingId.slice(trainingId.lastIndexOf("/") + 1), membershipId);
+    console.log(props.data.row.visitsAmount + " updatedVisitsAmount" + updatedVisitsAmount)
+    if (updatedVisitsAmount !== " " && updatedVisitsAmount.length !== 0){
+      props.updateTrainingMembership(props.data.id, trainingId, membershipId, updatedVisitsAmount)
+      setVisitsAmount(" ")
+    } 
+    else{
+      props.updateTrainingMembership(props.data.id, trainingId, membershipId, props.data.row.visitsAmount);
+    }
     handleClose();
   }
 
@@ -77,32 +105,34 @@ function EditTrainingMembership(props) {
           <DialogContent className='dialog'>
         <Stack spacing={2} mt={1}>
             <TextField label="Количество занятий в пакете" name="visitsAmount" autoFocus
-            variant="standard" value={membership.visitsAmount} 
-            onChange={handleChange}/>
+            variant="standard" value={training_membership.visitsAmount} 
+            onChange={handleChangeAmount}/>
             <FormControl fullWidth>
             <InputLabel>Тренировки</InputLabel>
              <Select
              name='training'
              autoFocus variant="standard"
              label="Тренировки"
-             onChange={(event) => { setTrainingId(event.target.value) }}>
+             value={training_membership.training}
+             onChange={handleChangeTraining}>
              {trainings.map(training => (
-               <MenuItem key={training._links.self.href}
-                value={training._links.self.href}>{"Тренировка № " + training._links.self.href.slice(training._links.self.href.lastIndexOf("/") + 1)
-                  + " дата и время проведения:" + training.trainingDateTime}</MenuItem>
+               <MenuItem key={training.idTraining}
+                value={training.idTraining}>{"Тренировка №" + training.idTraining
+                + ". Место проведения: " + training.complexFacility.facilityType}</MenuItem>
              ))}
             </Select>
             </FormControl>
             <FormControl fullWidth>
             <InputLabel>Абонементы</InputLabel>
              <Select
-             name='client'
+             name='sportComplexMembership'
              autoFocus variant="standard"
              label="Абонементы"
-             onChange={(event) => { setMembershipId(event.target.value) }}>
+             value={training_membership.sportComplexMembership}
+             onChange={handleChangeMembership}>
              {memberships.map(membership => (
                <MenuItem key={membership.idSportComplexMembership}
-                value={membership.idSportComplexMembership}>{membership.name}</MenuItem>
+                value={membership.idSportComplexMembership}>{"Абонемент №" + membership.idSportComplexMembership + ": " + membership.name}</MenuItem>
              ))}
             </Select>
             </FormControl>
