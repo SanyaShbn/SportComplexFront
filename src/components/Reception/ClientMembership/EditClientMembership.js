81905cc380f8730023a222040779fd695acd5172
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-04-03 00:41:38 +0300

    Update EditClientMembership.js

--- src/components/Reception/ClientMembership/EditClientMembership.js
+++ src/components/Reception/ClientMembership/EditClientMembership.js
@@ -16,6 +16,9 @@ function EditClientMembership(props) {
     const [memberships, setMemberships] = useState([]);
     const [clients, setClients] = useState([]);
     const [open, setOpen] = useState(false);
+    const [client_membership, setClientMembership] = useState({
+      sportComplexMembership: '', client: ''
+    });
   
     useEffect(() => {
       fetchClients();
@@ -23,30 +26,48 @@ function EditClientMembership(props) {
     }, []);
   
     const fetchClients = () => {
-      // const token = sessionStorage.getItem("jwt");
+      const token = sessionStorage.getItem("jwt");
       fetch(SERVER_URL + '/api/view_clients', {
-        // headers: { 'Authorization' : token }
+        headers: { 'Authorization' : token }
       })
       .then(response => response.json())
       .then(data => setClients(data))
       .catch(err => console.error(err));    
     }
   
-      const fetchMemberships = () => {
-          // const token = sessionStorage.getItem("jwt");
-          fetch(SERVER_URL + '/api/view_memberships', {
-            // headers: { 'Authorization' : token }
-          })
-          .then(response => response.json())
-          .then(data => setMemberships(data))
-          .catch(err => console.error(err));    
+    const fetchMemberships = () => {
+      const token = sessionStorage.getItem("jwt");
+      fetch(SERVER_URL + '/api/view_memberships', {
+        headers: { 'Authorization' : token }
+       })
+      .then(response => response.json())
+      .then(data => setMemberships(data))
+      .catch(err => console.error(err));    
     }
-
 
     const handleClickOpen = () => {  
-        setOpen(true);
+      let id_client = props.data.row.client.slice(props.data.row.client.indexOf("№") + 1, props.data.row.client.indexOf(":"))
+      let id_membership = props.data.row.sportComplexMembership.slice(props.data.row.sportComplexMembership.indexOf("№") + 1, 
+      props.data.row.sportComplexMembership.indexOf(":"))
+      setMembershipId(parseInt(id_membership))
+      setClientId(parseInt(id_client))
+      setClientMembership({
+        sportComplexMembership: parseInt(id_membership),
+        client: parseInt(id_client),
+       })  
+      setOpen(true);
     }
-      
+
+    const handleChangeMembership = (event) => {
+      setMembershipId(event.target.value) 
+      setClientMembership({...client_membership, 
+        [event.target.name]: event.target.value});
+    }
+    const handleChangeClient = (event) => {
+      setClientId(event.target.value)
+      setClientMembership({...client_membership, 
+        [event.target.name]: event.target.value});
+    }
 
   const handleClose = () => {
     setOpen(false);
@@ -72,10 +93,11 @@ function EditClientMembership(props) {
              name='client'
              autoFocus variant="standard"
              label="Клиенты"
-             onChange={(event) => { setClientId(event.target.value) }}>
+             value={client_membership.client}
+             onChange={handleChangeClient}>
              {clients.map(client => (
                <MenuItem key={client.idClient}
-                value={client.idClient}>{client.surName + " " + client.firstName + " " + client.patrSurName + 
+                value={client.idClient}>{"Клиент №" + client.idClient + ": " + client.surName + " " + client.firstName + " " + client.patrSurName + 
                 " (" + client.phoneNumber + ")"}</MenuItem>
              ))}
             </Select>
@@ -83,13 +105,14 @@ function EditClientMembership(props) {
             <FormControl fullWidth>
             <InputLabel>Абонементы</InputLabel>
              <Select
-             name='client'
+             name='sportComplexMembership'
              autoFocus variant="standard"
              label="Абонементы"
-             onChange={(event) => { setMembershipId(event.target.value) }}>
+             value={client_membership.sportComplexMembership}
+             onChange={handleChangeMembership}>
              {memberships.map(membership => (
                <MenuItem key={membership.idSportComplexMembership}
-                value={membership.idSportComplexMembership}>{membership.name}</MenuItem>
+                value={membership.idSportComplexMembership}>{"Абонемент №" + membership.idSportComplexMembership + ": " + membership.name}</MenuItem>
              ))}
             </Select>
             </FormControl>
