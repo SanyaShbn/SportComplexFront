f93281f83ff03ac50e731544259051f95e5b7eaa
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-04-13 01:25:05 +0300

    Update Home.js

--- src/components/MainPage/Home.js
+++ src/components/MainPage/Home.js
@@ -1,4 +1,5 @@
 import React,  { useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
 import 
 { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}
  from 'react-icons/bs'
@@ -11,6 +12,16 @@ const Home = ({ setSelectedLink, link }) => {
   useEffect(() => {
     setSelectedLink(link);
   }, []);
+
+  
+  const location = useLocation();
+
+  useEffect(() => {
+    window.history.pushState(null, document.title, window.location.href);
+    window.addEventListener('popstate', function(event) {
+      window.history.pushState(null, document.title, window.location.href);
+    });
+  }, [location]);
 
     const data = [
         {
@@ -61,34 +72,34 @@ const Home = ({ setSelectedLink, link }) => {
   return (
     <main className='main-container'>
         <div className='main-title'>
-            <h3>DASHBOARD</h3>
+            <h3>ИНФОРМАЦИОННАЯ ПАНЕЛЬ</h3>
         </div>
 
         <div className='main-cards'>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>PRODUCTS</h3>
+                    <h3>УСЛУГИ</h3>
                     <BsFillArchiveFill className='card_icon'/>
                 </div>
                 <h1>300</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>CATEGORIES</h3>
+                    <h3>КАТЕГОРИИ</h3>
                     <BsFillGrid3X3GapFill className='card_icon'/>
                 </div>
                 <h1>12</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>CUSTOMERS</h3>
+                    <h3>КЛИЕНТЫ</h3>
                     <BsPeopleFill className='card_icon'/>
                 </div>
                 <h1>33</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>ALERTS</h3>
+                    <h3>УВЕДОМЛЕНИЯ</h3>
                     <BsFillBellFill className='card_icon'/>
                 </div>
                 <h1>42</h1>
